5ab5e9d8c85da968c3cf1b4212e3ac11897221e0
Пирожков Евгений Евгеньевич (4060542) <eepirozhkov@dev.vtb.ru>
2025-01-16 11:10:53 +0300

    new project config added

---
+++ src/main/java/evgen/com/predictions/PredictionsApplication.java
@@ -0,0 +1,16 @@
+package evgen.com.predictions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class PredictionsApplication {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(PredictionsApplication.class, args);
+		System.out.println("Жотём");
+	}
+
+}
