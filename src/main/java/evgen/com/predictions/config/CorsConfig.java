6186f1cb25167919cdb60ef6197c25e0b2d9cf1e
Пирожков Евгений Евгеньевич (4060542) <eepirozhkov@dev.vtb.ru>
2025-01-29 12:17:16 +0300

    config/corsEdited

--- src/main/java/evgen/com/predictions/config/CorsConfig.java
+++ src/main/java/evgen/com/predictions/config/CorsConfig.java
@@ -1,18 +1,35 @@
 package evgen.com.predictions.config;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**") // Разрешить CORS для всех эндпоинтов
+//                .allowedOrigins("http://localhost:5172") // Разрешить запросы с этого origin
+//                .allowedMethods("GET", "POST", "PUT", "DELETE") // Разрешенные HTTP-методы
+//                .allowedHeaders("*") // Разрешить все заголовки
+//                .allowCredentials(true); // Разрешить передачу куки и авторизационных данных
+//    }
+//}
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Разрешить CORS для всех эндпоинтов
-                .allowedOrigins("http://localhost:5172") // Разрешить запросы с этого origin
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Разрешенные HTTP-методы
-                .allowedHeaders("*") // Разрешить все заголовки
-                .allowCredentials(true); // Разрешить передачу куки и авторизационных данных
+        @Bean
+        public WebMvcConfigurer corsConfigurer(){
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+                }
+            };
+        }
     }
-}
